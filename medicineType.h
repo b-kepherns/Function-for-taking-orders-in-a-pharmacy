deb1a3cf5f490732cd8c513a8eb250c34073ab6e
Brian Kepherns <58827331+b-kepherns@users.noreply.github.com>
2019-12-16 18:24:18 +0300

    Add files via upload

---
+++ medicineType.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+class medicineType
+{
+public:
+
+	void take_order(); //take order
+	void delete_order();//delete order
+	void modify();//modify order
+	void order_list();//display order list
+	void daily_summary();//display summary
+	void exit();//exit the menu
+	medicineType();
+};
+medicineType::medicineType()
+{
+}
+struct node //constructing a node
+{
+	int receipt_number;
+	std::string customername;
+	std::string date;
+	int quantity[10];
+	std::string type = { "OTC" };
+	int x, menu2[10];
+	double amount[10];
+	std::string medicinename[10]= { "Probiotics","Vitamin C(500mg)","Acid Free C(500mg)","Women'S Multivate","Marino Tablet","Maxi Cal Tablet",
+	"Amino Zinc Tablet","Burnex","Fabuloss 5","Royal Propollen" };
+	double medicine[10] = { 2.00,3.00,1.00,4.00,1.00,5.00,7.00,4.00,3.00,5.00 };
+	double total;
+
+	node* prev;
+	node* next;
+	node* link;
+
+
+}; int  *q, * temp; //pointer declaration
+node* start_ptr = NULL;
+node* head = NULL;
+node* last = NULL;
